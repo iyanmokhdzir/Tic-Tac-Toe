1d0c18930f975bc668f6929487443edc447d8dbb
Iyan Mokhdzir <iyan94@gmail.com>
2023-12-11 23:14:34 +0800

    added "New Game" button and display the correct message and button when game over

--- app.js
+++ app.js
@@ -4,220 +4,228 @@ let oMarkingInterval;
 
 //helper functions
 function reloadPage() {
-    location.reload();
+  location.reload();
 }
 
 function getRandomGrid() {
-    let randomGrid = Math.floor(Math.random() * 9);
-    return randomGrid.toString();
+  let randomGrid = Math.floor(Math.random() * 9);
+  return randomGrid.toString();
 }
 
 //
 window.onload = function () {
-    setupGrid();
-    document.getElementById("ok-button").addEventListener("click", addEventListenerToGrid);
-    document.getElementById("newGame-button").addEventListener("click", reloadPage);
-}
+  setupGrid();
+  document
+    .getElementById("ok-button")
+    .addEventListener("click", addEventListenerToGrid);
+};
 
 function setupGrid() {
-    for (let i = 0; i < 9; i++) {
-        const grid = document.createElement("div");
-        grid.id = i.toString();
-        document.getElementById("board").appendChild(grid);        
-    }    
+  for (let i = 0; i < 9; i++) {
+    const grid = document.createElement("div");
+    grid.id = i.toString();
+    document.getElementById("board").appendChild(grid);
+  }
 }
 
 function addEventListenerToGrid() {
-    document.getElementById("popup").style.display = "none";
+  document.getElementById("popup").style.display = "none";
 
-    for (let i = 0; i < 9; i++) {
-        let grid = document.getElementById(i);
-        grid.addEventListener("click", gameStart);
-        grid.isClicked = false;
-        grid.mark = "empty";
-    }
-    gameTurn = "user";    
+  for (let i = 0; i < 9; i++) {
+    let grid = document.getElementById(i);
+    grid.addEventListener("click", gameStart);
+    grid.isClicked = false;
+    grid.mark = "empty";
+  }
+  gameTurn = "user";
 }
 
 function gameStart() {
-    if (gameOver) {
-        return;
-    }
+  if (gameOver) {
+    return;
+  }
 
-    let x = document.createElement("img");
-    x.src = "./X.png";
+  let x = document.createElement("img");
+  x.src = "./X.png";
 
-    if (!this.isClicked && gameTurn == "user") {
-        this.appendChild(x);
-        this.isClicked = true;
-        this.mark = "x";
+  if (!this.isClicked && gameTurn == "user") {
+    this.appendChild(x);
+    this.isClicked = true;
+    this.mark = "x";
 
-        gameWinner();
-        gameTurn = "bot";
-        oMarkingInterval = setInterval(oMarking, 1000);
-    }
-    else if (this.isClicked && gameTurn == "user") {
-        alert("The selected tile is occupied. Please choose another tile.")
-    }
-    else {
-        alert("Please wait for your turn.");
-    }
+    gameWinner();
+    gameTurn = "bot";
+    oMarkingInterval = setInterval(oMarking, 1000);
+  } else if (this.isClicked && gameTurn == "user") {
+    alert("The selected tile is occupied. Please choose another tile.");
+  } else {
+    alert("Please wait for your turn.");
+  }
 }
 
 function oMarking() {
-    if (gameOver) {
-        return;
+  if (gameOver) {
+    return;
+  }
+
+  clearInterval(oMarkingInterval);
+
+  let randomGrid = getRandomGrid();
+
+  if (gameTurn == "bot") {
+    let o = document.createElement("img");
+    o.src = "./O.png";
+
+    let botGrid = document.getElementById(randomGrid);
+
+    if (botGrid.isClicked) {
+      oMarking();
+    } else {
+      botGrid.appendChild(o);
+      botGrid.isClicked = true;
+      botGrid.mark = "o";
+      gameWinner();
+      gameTurn = "user";
     }
-
-    clearInterval(oMarkingInterval);
-
-    let randomGrid = getRandomGrid();
-
-    if (gameTurn == "bot") {
-        let o = document.createElement("img");
-        o.src = "./O.png";
-
-        let botGrid = document.getElementById(randomGrid);
-
-        if (botGrid.isClicked) {
-            oMarking();
-        }
-        else {
-            botGrid.appendChild(o);
-            botGrid.isClicked = true;
-            botGrid.mark = "o";
-            gameWinner();
-            gameTurn = "user";
-        }
-    }
+  }
 }
 
 function gameWinner() {
-    let tile0 = document.getElementById(0);
-    let tile1 = document.getElementById(1);
-    let tile2 = document.getElementById(2);
-    let tile3 = document.getElementById(3);
-    let tile4 = document.getElementById(4);
-    let tile5 = document.getElementById(5);
-    let tile6 = document.getElementById(6);
-    let tile7 = document.getElementById(7);
-    let tile8 = document.getElementById(8);
+  let tile0 = document.getElementById(0);
+  let tile1 = document.getElementById(1);
+  let tile2 = document.getElementById(2);
+  let tile3 = document.getElementById(3);
+  let tile4 = document.getElementById(4);
+  let tile5 = document.getElementById(5);
+  let tile6 = document.getElementById(6);
+  let tile7 = document.getElementById(7);
+  let tile8 = document.getElementById(8);
 
-    let popupElement = document.getElementById("popup");
+  let winner = "";
 
-    let winner = "";
+  //0,1,2
+  if (tile0.mark == "x" && tile1.mark == "x" && tile2.mark == "x") {
+    gameOver = true;
+    winner = "user";
+    winnerPopup(winner);
+  } else if (tile0.mark == "o" && tile1.mark == "o" && tile2.mark == "o") {
+    gameOver = true;
+    winner = "bot";
+    winnerPopup(winner);
+  }
+  //0,3,6
+  else if (tile0.mark == "x" && tile3.mark == "x" && tile6.mark == "x") {
+    gameOver = true;
+    winner = "user";
+    winnerPopup(winner);
+  } else if (tile0.mark == "o" && tile3.mark == "o" && tile6.mark == "o") {
+    gameOver = true;
+    winner = "bot";
+    winnerPopup(winner);
+  }
+  //0,4,8
+  else if (tile0.mark == "x" && tile4.mark == "x" && tile8.mark == "x") {
+    gameOver = true;
+    winner = "user";
+    winnerPopup(winner);
+  } else if (tile0.mark == "o" && tile4.mark == "o" && tile8.mark == "o") {
+    gameOver = true;
+    winner = "bot";
+    winnerPopup(winner);
+  }
+  //1,4,7
+  else if (tile1.mark == "x" && tile4.mark == "x" && tile7.mark == "x") {
+    gameOver = true;
+    winner = "user";
+    winnerPopup(winner);
+  } else if (tile1.mark == "o" && tile4.mark == "o" && tile7.mark == "o") {
+    gameOver = true;
+    winner = "bot";
+    winnerPopup(winner);
+  }
+  //2,5,8
+  else if (tile2.mark == "x" && tile5.mark == "x" && tile8.mark == "x") {
+    gameOver = true;
+    winner = "user";
+    winnerPopup(winner);
+  } else if (tile2.mark == "o" && tile5.mark == "o" && tile8.mark == "o") {
+    gameOver = true;
+    winner = "bot";
+    winnerPopup(winner);
+  }
+  //2,4,6
+  else if (tile2.mark == "x" && tile4.mark == "x" && tile6.mark == "x") {
+    gameOver = true;
+    winner = "user";
+    winnerPopup(winner);
+  } else if (tile2.mark == "o" && tile4.mark == "o" && tile6.mark == "o") {
+    gameOver = true;
+    winner = "bot";
+    winnerPopup(winner);
+  }
+  //3,4,5
+  else if (tile3.mark == "x" && tile4.mark == "x" && tile5.mark == "x") {
+    gameOver = true;
+    winner = "user";
+    winnerPopup(winner);
+  } else if (tile3.mark == "o" && tile4.mark == "o" && tile5.mark == "o") {
+    gameOver = true;
+    winner = "bot";
+    winnerPopup(winner);
+  }
+  //6,7,8
+  else if (tile6.mark == "x" && tile7.mark == "x" && tile8.mark == "x") {
+    gameOver = true;
+    winner = "user";
+    winnerPopup(winner);
+  } else if (tile6.mark == "o" && tile7.mark == "o" && tile8.mark == "o") {
+    gameOver = true;
+    winner = "bot";
+    winnerPopup(winner);
+  } else if (
+    tile0.mark != "empty" &&
+    tile1.mark != "empty" &&
+    tile2.mark != "empty" &&
+    tile3.mark != "empty" &&
+    tile4.mark != "empty" &&
+    tile5.mark != "empty" &&
+    tile6.mark != "empty" &&
+    tile7.mark != "empty" &&
+    tile8.mark != "empty"
+  ) {
+    gameOver = true;
+    winner = "none";
+    winnerPopup(winner);
+  } else {
+    gameOver = false;
+  }
 
-    //0,1,2
-    if (tile0.mark == "x" && tile1.mark == "x" && tile2.mark == "x") {
-        gameOver = true;
-        winner = "user"; 
-        displayWinnerPopup(winner);       
-    }
-    else if (tile0.mark == "o" && tile1.mark == "o" && tile2.mark == "o") {
-        gameOver = true;
-        winner = "bot";
-        displayWinnerPopup(winner);
-    }
-    //0,3,6
-    else if (tile0.mark == "x" && tile3.mark == "x" && tile6.mark == "x") {
-        gameOver = true;
-        winner = "user";
-        displayWinnerPopup(winner); 
-    }
-    else if (tile0.mark == "o" && tile3.mark == "o" && tile6.mark == "o") {
-        gameOver = true;
-        winner = "bot";
-        displayWinnerPopup(winner);
-    }
-    //0,4,8
-    else if (tile0.mark == "x" && tile4.mark == "x" && tile8.mark == "x") {
-        gameOver = true;
-        winner = "user";
-        displayWinnerPopup(winner); 
-    }
-    else if (tile0.mark == "o" && tile4.mark == "o" && tile8.mark == "o") {
-        gameOver = true;
-        winner = "bot";
-        displayWinnerPopup(winner);
-    }
-    //1,4,7
-    else if (tile1.mark == "x" && tile4.mark == "x" && tile7.mark == "x") {
-        gameOver = true;
-        winner = "user";
-        displayWinnerPopup(winner); 
-    }
-    else if (tile1.mark == "o" && tile4.mark == "o" && tile7.mark == "o") {
-        gameOver = true;
-        winner = "bot";
-        displayWinnerPopup(winner);
-    }
-    //2,5,8
-    else if (tile2.mark == "x" && tile5.mark == "x" && tile8.mark == "x") {
-        gameOver = true;
-        winner = "user";
-        displayWinnerPopup(winner); 
-    }
-    else if (tile2.mark == "o" && tile5.mark == "o" && tile8.mark == "o") {
-        gameOver = true;
-        winner = "bot";
-        displayWinnerPopup(winner);
-    }
-    //2,4,6
-    else if (tile2.mark == "x" && tile4.mark == "x" && tile6.mark == "x") {
-        gameOver = true;
-        winner = "user";
-        displayWinnerPopup(winner); 
-    }
-    else if (tile2.mark == "o" && tile4.mark == "o" && tile6.mark == "o") {
-        gameOver = true;
-        winner = "bot";
-        displayWinnerPopup(winner);
-    }
-    //3,4,5
-    else if (tile3.mark == "x" && tile4.mark == "x" && tile5.mark == "x") {
-        gameOver = true;
-        winner = "user";
-        displayWinnerPopup(winner); 
-    }
-    else if (tile3.mark == "o" && tile4.mark == "o" && tile5.mark == "o") {
-        gameOver = true;
-        winner = "bot";
-        displayWinnerPopup(winner);
-    }
-    //6,7,8
-    else if (tile6.mark == "x" && tile7.mark == "x" && tile8.mark == "x") {
-        gameOver = true;
-        winner = "user";
-        displayWinnerPopup(winner); 
-    }
-    else if (tile6.mark == "o" && tile7.mark == "o" && tile8.mark == "o") {
-        gameOver = true;
-        winner = "bot";
-        displayWinnerPopup(winner);
-    }
-    else if (tile0.mark!="empty" && tile1.mark!="empty" && tile2.mark!="empty" && tile3.mark!="empty" && tile4.mark!="empty" && tile5.mark!="empty" && tile6.mark!="empty" && tile7.mark!="empty" && tile8.mark!="empty") {
-        gameOver = true;
-        winner = "none";
-        displayWinnerPopup(winner);
-    }
-    else {
-        gameOver = false;
-    }
+  function winnerPopup(winner) {
+    let popup = document.getElementById("popup");
+    let popupText = document.getElementById("popup-text");
+    let okButton = document.getElementById("ok-button");
+    let newGameButton = document.getElementById("newGame-button");
 
-    function displayWinnerPopup(winner) {
-        if (winner == "user") {            
-            popupElement.innerHTML = "CONGRATULATIONS, YOU WIN!";
-            popupElement.style.display = "flex";   
-        }
-        else if (winner == "bot") {
-            popupElement.innerHTML = "SORRY, YOU LOSE.";
-            popupElement.style.display = "flex";
-        }
-        else {
-            popupElement.innerHTML = "NO WINNERS.";
-            popupElement.style.display = "flex";
-        }
+    if (winner == "user") {
+      popup.style.display = "flex";
+      popupText.innerHTML = "CONGRATULATIONS, YOU WIN!";
+      okButton.style.display = "none";
+      newGameButton.style.display = "flex";
+      newGameButton.addEventListener("click", reloadPage);
+    } else if (winner == "bot") {
+      popup.style.display = "flex";
+      popupText.innerHTML = "SORRY, YOU LOSE!";
+      okButton.style.display = "none";
+      newGameButton.style.display = "flex";
+      newGameButton.addEventListener("click", reloadPage);
+    } else {
+      popup.style.display = "flex";
+      popupText.innerHTML = "NO WINNERS.";
+      okButton.style.display = "none";
+      newGameButton.style.display = "flex";
+      newGameButton.addEventListener("click", reloadPage);
     }
+  }
 
-    return gameOver;
+  return gameOver;
 }
